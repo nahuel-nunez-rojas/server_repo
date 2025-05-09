db93bf045c1e48545753c51ef2d592fd889f025b
Nahuel Nuñez Rojas <167609595+nahuel-nunez-rojas@users.noreply.github.com>
2025-05-09 17:30:22 +0200

    Create actualizar_portainer.sh
    actualizar version portainer

---
+++ actualizar_portainer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "🛑 Deteniendo contenedor Portainer..."
+docker stop portainer
+
+echo "🧼 Eliminando contenedor Portainer..."
+docker rm portainer
+
+echo "⬇️ Descargando última imagen de Portainer..."
+docker pull portainer/portainer-ce:latest
+
+echo "🚀 Iniciando nuevo contenedor Portainer..."
+docker run -d \
+  -p 8000:8000 \
+  -p 9000:9000 \
+  -p 9443:9443 \
+  --name portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /home/administrator/docker/portainer:/data \
+  portainer/portainer-ce:latest
+
+echo "✅ Portainer actualizado y corriendo."
